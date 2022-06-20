7e7542a9450bbeb3c61d65e8fa99af7493cc27d2
Zeinab Khosravinia <z.khosravinia@gmail.com>
2022-06-20 23:01:04 +0430

    Initialize project

---
+++ LanguageGame/LanguageGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LanguageGame
+//
+//  Created by Zeinab Khosravinia on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
