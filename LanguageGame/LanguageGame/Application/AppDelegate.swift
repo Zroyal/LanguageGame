5583165c2a9aac3442be118126446c6d5b37b4ef
Zeinab Khosravinia <z.khosravinia@gmail.com>
2022-06-21 13:50:08 +0430

    Folding the project based on clean architecture

---
+++ LanguageGame/LanguageGame/Application/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  LanguageGame
+//
+//  Created by Zeinab Khosravinia on 6/20/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.overrideUserInterfaceStyle = .light
+                
+        let vc = ViewController()
+
+        window?.rootViewController = vc
+        
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+
+}
+
--- LanguageGame/LanguageGame/Presentation/Game/View/ViewController.swift
+++ LanguageGame/LanguageGame/Presentation/Game/View/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
 }
 
